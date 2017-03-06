f2e81fb65721a1c4a4b0d0e505fed68f5a534c6e
卫川 <weiguo.zhuwg@alipay.com>
2017-03-06 20:43:40 +0800

    fix login st

--- w7udots/rcp_id_rsa_pub.sh
+++ w7udots/rcp_id_rsa_pub.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 
+iplistf=~/w7udots/ssh-helper/iplist.cfg
 while getopts "h:u:p:c:t:" opt; do
     case $opt in
         h) t_host="$OPTARG"
@@ -32,11 +33,11 @@ if [ ${#t_type} -lt 1 ]; then
 fi
 
 # ssh-add -K /Users/weiguo/.ssh/id_rsa
-if [ `grep $t_host ~/w7udots/iplist.cfg | wc -l` -eq 0 ]; then
+if [ `grep $t_host $iplistf | wc -l` -eq 0 ]; then
     cat ~/.ssh/id_rsa.pub | (ssh -p $t_port $t_user@$t_host "cat >> ~/.ssh/authorized_keys")
     # cat ~/.ssh/id_rsa.pub | (ssh -p $t_port $t_user@$t_host "cat >> ~/.ssh/known_hosts")
-    if [ $? -eq 0 -a `grep $t_host ~/w7udots/iplist.cfg | wc -l` -eq 0 ]; then
-        echo "$t_host\t$t_user\t$t_port\t$t_type\t#$t_comment" >> ~/w7udots/ssh-helper/iplist.cfg
+    if [ $? -eq 0 -a `grep $t_host $iplistf | wc -l` -eq 0 ]; then
+        echo "$t_host\t$t_user\t$t_port\t$t_type\t#$t_comment" >> $iplistf
         echo "w"
     fi
 else
