9f034b866d1022bed73f955c950d884ec41fc363
卫川 <weiguo.zhuwg@antfin.com>
2017-10-25 15:46:52 +0800

    period update

--- w7udots/update.sh
+++ w7udots/update.sh
@@ -1,9 +1,15 @@
 cd "$(brew --repo)"
+echo "[0] git brew"
 git prune && git gc && git fetch && git reset --hard origin/master
+echo "\n[1] brew update && brew upgrade && brew cleanup"
 brew update && brew upgrade && brew cleanup
+echo "\n[2] sh /Users/weiguo/w7udots/oh-my-zsh/tools/upgrade.sh"
 sh /Users/weiguo/w7udots/oh-my-zsh/tools/upgrade.sh
 # cd /Users/weiguo/playground/XX-Net && git pull --rebase --stat origin master
 
-npm update
-npm -g outdated
+echo "\n[3] tnpm update"
+tnpm update
+echo "\n[4] tnpm -g outdated"
+tnpm -g outdated
 # npm -g install webpack
+# npm update -g npm
