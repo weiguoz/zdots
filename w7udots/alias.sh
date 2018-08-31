38f69977cc1339b35f58884b18d83670e97ae91f
卫川 <weiguo.zhuwg@antfin.com>
2018-08-31 14:44:55 +0800

    ideavimrc

--- w7udots/alias.sh
+++ w7udots/alias.sh
@@ -23,8 +23,8 @@ alias fd='find . -type d -iname'
 alias fn='find . ! -wholename'
 # login +++
 alias cos="/Users/weiguo/w7udots/ssh-helper/bastion.exp"
-alias sin="LC_ALL='zh_CN.GBK' luit /Users/weiguo/w7udots/ssh-helper/bastion_with_cookie.exp"  # running in tmux with luit
-# alias sin="export LC_ALL='zh_CN.GBK' && ${DOTS}/ssh-helper/bastion_with_cookie.exp; export LC_ALL='en_US.UTF-8'"
+alias sin='LC_ALL="zh_CN.GBK" luit /Users/weiguo/w7udots/ssh-helper/bastion_with_cookie.exp'  # running in tmux with luit
+alias sin2="export LC_ALL='zh_CN.GBK' && ${DOTS}/ssh-helper/bastion_with_cookie.exp; export LC_ALL='en_US.UTF-8'"
 alias tan="${DOTS}/ssh-helper/sin.sh"
 alias rsa_spawn="sh ${DOTS}/rcp_id_rsa_pub.sh"
 # login ---
