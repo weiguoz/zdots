063ae2d067cdbc31764fe780a1a879fabf6bd0f2
weiguo <myxjtu@gmail.com>
2019-04-04 14:03:17 +0800

    vim status replaced by vim-airline
    add fzf cmd search
    
    修改快捷键，否则会拖慢相近的命令。如修改n为，，不改的话next查找会卡顿
    
    zshrc dedup history
    
    tiny fix
    
    add fzf export
    
    add v from https://github.com/rupa/v, so remove alias v=vim
    
    better tx

--- w7udots/shrc_profile.sh
+++ w7udots/shrc_profile.sh
@@ -8,6 +8,8 @@ import() {
 
 DOTS='/Users/weiguo/w7udots'
 
+
+import ~/.fzf.zsh # https://github.com/junegunn/fzf
 #eval $(thefuck --alias)
 import ${DOTS}/z/z.sh # https://github.com/rupa/z
 import ${DOTS}/exports.sh
@@ -15,4 +17,4 @@ import ${DOTS}/settings.sh
 import ${DOTS}/init.sh
 # import /Library/Developer/CommandLineTools/usr/share/git-core/git-prompt.sh
 # export PS1='${ret_status}%{$fg_bold[green]%}%p %{$fg[cyan]%}%~ %{$fg_bold[blue]%}$(git_prompt_info)%{$fg_bold[blue]%} % %{$reset_color%}'
-uptime
+# uptime
