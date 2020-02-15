1365613ba3d5d3538f91094d4fcfbd5a52675957
w7u <myxjtu@gmail.com>
2020-02-15 15:07:16 +0800

    add git fzf

---
+++ w7udots/gits/git_fzf.sh
@@ -0,0 +1,47 @@
+# git log show with fzf
+#
+# https://gist.github.com/tamphh/3c9a4aa07ef21232624bacb4b3f3c580/edit
+# https://asciinema.org/a/257939
+#
+
+# git log show with fzf
+gli() {
+
+  # param validation
+  if [[ ! `git log -n 1 $@ | head -n 1` ]] ;then
+    return
+  fi
+
+  # filter by file string
+  local filter
+  # param existed, git log for file if existed
+  if [ -n $@ ] && [ -f $@ ]; then
+    filter="-- $@"
+  fi
+
+  # git command
+  local gitlog=(
+    git log
+    --graph --color=always
+    --abbrev=7
+    --format='%C(auto)%h %an %C(blue)%s %C(yellow)%cr'
+    $@
+  )
+
+  # fzf command
+  local fzf=(
+    fzf
+    --ansi --no-sort --reverse --tiebreak=index
+    --preview "f() { set -- \$(echo -- \$@ | grep -o '[a-f0-9]\{7\}'); [ \$# -eq 0 ] || git show --color=always \$1 $filter; }; f {}"
+    --bind "ctrl-q:abort,ctrl-m:execute:
+                (grep -o '[a-f0-9]\{7\}' | head -1 |
+                xargs -I % sh -c 'git show --color=always % $filter | less -R') << 'FZF-EOF'
+                {}
+                FZF-EOF"
+   --preview-window=right:65%\
+       --height 70%
+  )
+
+  # piping them
+  $gitlog | $fzf
+}
