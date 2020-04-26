2d8c008dad545c13d0672838341e205fc21e217e
w7u <wg.z@qq.com>
2020-04-26 16:41:21 +0800

    add proxy for terminal

---
+++ w7udots/sh/proxy.sh
@@ -0,0 +1,21 @@
+my_prx() {
+    if [ $# -eq 0 ]; then
+        echo "Bad command"
+    elif [ $1 = "on" ]; then
+        server_tag=$2
+        proxy=$(lsof -i -n -P | grep LISTEN |grep ${server_tag} | tr -s ' ' | cut -d' ' -f 9)
+        if ([ -z "$proxy" ]); then
+            echo "Can't find a proxy server"
+        else
+            export http_proxy=$proxy
+            export https_proxy=$proxy
+            echo "Terminal proxy is: $proxy"
+        fi
+    elif [ $1 = "off" ]; then
+        unset http_proxy
+        unset https_proxy
+        echo 'Shutdown proxy on terminal'
+    else
+        echo "Bad command: $1"
+    fi
+}
