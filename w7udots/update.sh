0109816848cf628df6a2109fdcb6add5d64b086d
weiguoz <wg.z@qq.com>
2020-12-11 13:26:48 +0800

    shortkey keybind

--- w7udots/update.sh
+++ w7udots/update.sh
@@ -5,13 +5,14 @@ git gc && git fetch && git reset --hard origin/master
 echo "\n[1] brew update && brew upgrade && brew cleanup"
 brew update && brew upgrade && brew cleanup
 echo "\n[2] update ohmyzsh"
-echo "+---[2.1] sh ${DOTS}/ohmyzsh/tools/upgrade.sh"
-sh ${DOTS}/ohmyzsh/tools/upgrade.sh
-# cd /path/to/playground/XX-Net && git pull --rebase --stat origin master
+echo "+---[2.1] git pull ohmyzsh && sh ohmyzsh/tools/upgrade.sh"
+cd ${DOTS}/ohmyzsh && git pull -p && sh ${DOTS}/ohmyzsh/tools/upgrade.sh
 echo "+---[2.2] git pull zsh-autosuggestions"
 cd ${DOTS}/ohmyzsh/custom/plugins/zsh-autosuggestions && git pull -p
 echo "+---[2.3] git pull zsh-syntax-highlighting"
 cd ${DOTS}/ohmyzsh/custom/plugins/zsh-syntax-highlighting && git pull -p
+
+# cd /path/to/playground/XX-Net && git pull --rebase --stat origin master
 
 # echo "\n[3] tnpm update"
 # tnpm update
