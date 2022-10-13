87157fb5c919302e61eeaaab5ff6fb903442b747
weiguoz <wg.z@qq.com>
2022-10-13 21:45:16 +0800

    fix bug: fzf-rg

--- zdots/sh/fzf.sh
+++ zdots/sh/fzf.sh
@@ -61,18 +61,18 @@ pods() {
 # refer: https://github.com/junegunn/fzf/blob/master/ADVANCED.md#switching-to-fzf-only-search-mode
 my_fzf_rg() {
     INITIAL_QUERY="${*:-}"
-	rgcmd='rg --column --line-number --multiline --no-heading --color=always --smart-case --colors "path:fg:190,220,255" --colors "line:fg:128,128,128"'
+	RG_PREFIX='rg --column --line-number --multiline --no-heading --color=always --smart-case --colors "path:fg:190,220,255" --colors "line:fg:128,128,128"'
     local selected=$(
         fzf --ansi --sort --phony \
             --color "hl:-1:underline,hl+:-1:underline:reverse" \
             --disabled --query "$INITIAL_QUERY" \
-			--bind "change:reload:sleep 0.1; ${rgcmd} {q} || true" \
+			--bind "change:reload:sleep 0.1; $RG_PREFIX {q} || true" \
             --bind "ctrl-b:unbind(change,ctrl-b)+change-prompt(2. fzf> )+enable-search+clear-query+rebind(ctrl-o)" \
             --bind "ctrl-o:unbind(ctrl-o)+change-prompt(1. rg> )+disable-search+reload($RG_PREFIX {q} || true)+rebind(change,ctrl-b)" \
             --prompt '1. rg> ' \
             --delimiter : \
             --header 'ctrl-o (rg mode, precision content) | ctrl-b (fzf mode, fuzzy title)' \
-            --preview-window up:follow \
+            --preview-window 'up,60%,border-bottom,+{2}+3/3,~3' \
             --preview="[[ ! -z {q} ]] && [[ ! -z {1} ]] && bat {1} --color=always --theme='Monokai Extended Bright' --style=numbers,changes --italic-text=always --highlight-line={2}"
 	)
     # split to an array method1: local a=("${(@s/:/)selected}") && [ -f "${a[1]}" ] && vim "${a[1]}" "+${a[2]}"
