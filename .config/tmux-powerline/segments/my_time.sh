1ceb49c1c0314910ad2567c536e01360115936c5
weiguoz <wg.z@qq.com>
2024-07-15 17:47:30 +0800

    the European Cup has ended, remove Berlin tz

--- .config/tmux-powerline/segments/my_time.sh
+++ .config/tmux-powerline/segments/my_time.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 run_segment() {
-    mm=$(( ($(date +%s) / 120) % 3 ))
-    if [ $mm -eq 0 ]; then
-        mydt=$(TZ="Europe/Berlin" date +"%a %H:%M")
-        echo "Berlin, $mydt"
-    elif [ $mm -eq 1 ]; then
-        date +"%a %H:%M"
-    else
+    mm=$(( ($(date +%s) / 60) % 4 ))
+    if [ $mm -lt 1 ]; then
         mydt=$(TZ="America/New_York" date +"%a %H:%M")
         echo "NY, $mydt"
+    elif [ $mm -lt 2 ]; then
+        mydt=$(TZ="Europe/London" date +"%a %H:%M")
+        echo "Arsenal, $mydt"
+    else
+        date +"%a %H:%M"
     fi
     return 0
 }
