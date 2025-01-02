c8de2582ba33b822529ca14626c188703802559a
weiguoz <wg.z@qq.com>
2025-01-02 16:12:53 +0800

    upgrade vim.lazy.bookmark plugin

--- .config/nvim/lua/plugins/bookmarks.lua
+++ .config/nvim/lua/plugins/bookmarks.lua
@@ -1,19 +1,18 @@
 return {
   "LintaoAmons/bookmarks.nvim", -- replace 'MattesGroeger/vim-bookmarks'
-  tag = "v1.4.2",
+  tag = "v2.4.0",
   dependencies = {
+    { "kkharji/sqlite.lua" },
     { "nvim-telescope/telescope.nvim" },
     { "stevearc/dressing.nvim" },        -- optional
   },
   config = function()
     require("bookmarks").setup({
-      json_db_path = vim.fn.stdpath("config") .. "/bookmarks.db.json",
+      -- json_db_path = vim.fn.stdpath("config") .. "/bookmarks.db.json",
       signs = {
-        mark = { icon = "♥", color = "red", line_bg = "#572626" },
+        mark = { icon = "📌", color = "red", line_bg = "#572626" } -- 󰃁
       },
-      picker = {
-        sort_by = "last_visited",
-      },
+      picker = { sort_by = "last_visited", sorting_strategy = "descending" }, -- NOTE: looks like it's not working
     })
 
     -- migrated from keymaps.lua vim.keymap.set({ "n", "v" }, "mm", "<cmd>BookmarksMark<cr>", { desc = "Mark current line into active BookmarkList." })
