30d0567aa8413bd477aef46affe2335b3c16dce0
weiguoz <wg.z@qq.com>
2025-01-12 14:10:32 +0800

    modify fzf[pods] key

--- zdots/sh/fzf.sh
+++ zdots/sh/fzf.sh
@@ -50,10 +50,10 @@ pods() {
   FZF_DEFAULT_COMMAND="kubectl get pods --all-namespaces" \
     fzf --info=inline --layout=reverse --header-lines=1 \
         --prompt "$(kubectl config current-context | sed 's/-context$//')> " \
-        --header $'↩︎: [tail log]    ^-f: [logs]    ^-i: [describe]    ^-o[aboard]' \
+        --header $'↩︎: [tail log]    ^-f: [logs]    ^-k: [describe]    ^-o[aboard]' \
         --bind 'enter:execute:sh -c "kubectl logs --follow --all-containers --tail=10000 --namespace {1} {2}" > /dev/tty' \
         --bind 'ctrl-f:execute:sh -c "kubectl logs --since=4h --all-containers --namespace {1} {2} | nvim - +" > /dev/tty' \
-        --bind 'ctrl-i:execute:sh -c "kubectl describe pods --namespace {1} {2} | nvim" > /dev/tty' \
+        --bind 'ctrl-k:execute:sh -c "kubectl describe pods --namespace {1} {2} | nvim" > /dev/tty' \
         --bind 'ctrl-o:execute:kubectl exec -it --namespace {1} {2} -- sh > /dev/tty' \
         --preview-window up:follow \
         --preview 'kubectl logs --follow --all-containers --tail=10000 --namespace {1} {2}' "$@"
