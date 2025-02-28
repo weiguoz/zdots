a5c3be4ad3bbe5943af9c59763fddfd4dd48a3be
weiguoz <wg.z@qq.com>
2025-02-28 22:14:53 +0800

    vim.cmp c-f to accept lsp.completion but not c-y as usual

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -21,7 +21,7 @@ return {
       mapping = {
         ['<C-n>'] = cmp.mapping.select_next_item(),
         ['<C-p>'] = cmp.mapping.select_prev_item(),
-        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-f>'] = cmp.mapping.confirm({ select = true }),
         -- ['<C-Space>'] = cmp.mapping.complete(),
         ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
