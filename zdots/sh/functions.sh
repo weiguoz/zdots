6f1dfefe727b90246bfbe33629711d000b177035
weiguoz <wg.z@qq.com>
2025-03-21 18:40:41 +0800

    adjust ghostty font size and alias mysql_config_editor -> mysqlc

--- zdots/sh/functions.sh
+++ zdots/sh/functions.sh
@@ -88,3 +88,80 @@ ff () {
         find . -type f \( $(printf '! -name %q -o ' "$@") -false \) 2> /dev/null
     fi
 }
+
+mysqlc() {
+    mysql_config_editor print --all | awk '
+/^\[/ {
+    if (section != "" && host != "") {
+        port = (port == "") ? "3306" : port
+        gsub(/^\[/, "", section)
+        gsub(/\]$/, "", section)
+        printf "%s|addr=\"%s:%s\"|user=\"%s\"\n", section, host, port, user
+    }
+    section=$0
+    user=host=port=""
+    next
+}
+
+/user *=/ {
+    split($0, a, "=")
+    user=a[2]
+    gsub(/[" ]/, "", user)
+}
+/host *=/ {
+    split($0, a, "=")
+    host=a[2]
+    gsub(/[" ]/, "", host)
+}
+/port *=/ {
+    split($0, a, "=")
+    port=a[2]
+    gsub(/[" ]/, "", port)
+}
+
+END {
+    if (section != "" && host != "") {
+        port = (port == "") ? "3306" : port
+        gsub(/^\[/, "", section)
+        gsub(/\]$/, "", section)
+        printf "%s|addr=\"%s:%s\"|user=\"%s\"\n", section, host, port, user
+    }
+}
+' | sort | awk -F'\\|' '
+BEGIN {
+    colors[0]  = "\033[1;34m"  # bright red
+    colors[1] = "\033[1;32m"   # bright gray
+    colors[2]  = "\033[1;33m"  # bright yellow
+    colors[3]  = "\033[1;31m"  # bright blue
+    colors[4]  = "\033[1;35m"  # bright magenta
+    colors[5]  = "\033[1;36m"  # bright cyan
+    colors[6]  = "\033[0;31m"  # red
+    colors[7] = "\033[1;94m"   # bright light blue
+    colors[8]  = "\033[0;33m"  # yellow
+    colors[9]  = "\033[0;34m"  # blue
+    colors[10] = "\033[0;35m"  # magenta
+    colors[11] = "\033[0;36m"  # cyan
+
+    defaultColor = "\033[0m"
+    valColor = "\033[1;90m"
+    reset = "\033[0m"
+}
+{
+    sectionName = $1
+    split(sectionName, parts, ".")
+    prefix = (length(parts) > 1) ? parts[1] : "_no_prefix_"
+
+    if (!(prefix in prefixColorMap)) {
+        prefixColorMap[prefix] = (prefix == "_no_prefix_") ? defaultColor : colors[colorIndex++ % length(colors)]
+    }
+
+    color = prefixColorMap[prefix]
+    section = "[" color sectionName reset "]"
+
+    gsub(/addr=/, valColor "addr=" reset)
+    gsub(/user=/, valColor "user=" reset)
+
+    print section "\t" $2 ",\t" $3
+}
+'
+}
