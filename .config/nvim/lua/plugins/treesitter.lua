a722ace8e283f2dfbb8871e7feb441acac1edcab
weiguoz <wg.z@qq.com>
2025-06-14 11:54:34 +0800

    neovim add nvim-treesitter-context
    difference with dropbar.nvim

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,26 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        event = { "BufReadPost", "BufNewFile" },
 
-    config = function()
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = { "lua", "go", "c", "cpp", "java", "python", "bash", "sql", "thrift", "json", "jsonnet", "yaml", "toml", "tmux", "markdown", "rust", "javascript", "typescript", "html" },
-            highlight = { enable = true, additional_vim_regex_highlighting = false },
-            indent = { enable = true },
-        })
-    end,
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = { "lua", "go", "c", "cpp", "java", "python", "bash", "sql", "thrift", "json", "jsonnet", "yaml", "toml", "tmux", "markdown", "rust", "javascript", "typescript", "html" },
+                highlight = { enable = true, additional_vim_regex_highlighting = false },
+                indent = { enable = true },
+            })
+        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-context",
+        event = { "BufReadPost", "BufNewFile" },
+        config = function()
+            require("treesitter-context").setup({ max_lines = 4 })
+
+            vim.keymap.set("n", "[c", function()
+                require("treesitter-context").go_to_context(vim.v.count1)
+            end, { silent = true, desc = "treesitter-context: up" })
+        end,
+    },
 }
