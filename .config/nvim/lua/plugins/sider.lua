dfc7a74f7c9f181a57ef9d0841055b942dee8bc1
weiguoz <wg.z@qq.com>
2025-07-03 16:14:14 +0800

    nvim open sidebar while coding

--- .config/nvim/lua/plugins/sider.lua
+++ .config/nvim/lua/plugins/sider.lua
@@ -2,7 +2,8 @@ return {
     {
         'sidebar-nvim/sidebar.nvim', -- left
         config = function()
-            require("sidebar-nvim").setup({
+            local sidebar = require("sidebar-nvim")
+            sidebar.setup({
                 open = false,
                 initial_width = 30,
                 -- files
@@ -13,6 +14,18 @@ return {
                     attach_shell = "/bin/sh", show_all = true, interval = 5000,
                 },
                 todos = { ignored_paths = { "~" } },
+            })
+
+            local sidebar_opened_once = false
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = { "go", "c", "cpp", "python", "javascript",
+                    "typescript", "rust", "sql", "thrift", "lua", "sh" },
+                callback = function()
+                    if not sidebar_opened_once and not sidebar.is_open() then
+                        sidebar_opened_once = true
+                        vim.defer_fn(function() sidebar.open() end, 100) -- 延迟 100ms 调用，以避免初始化冲突
+                    end
+                end,
             })
         end
     },
