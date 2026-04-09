31b94dffad0a43c2e5151ab664a35d51494ba87b
weiguoz <wg.z@qq.com>
2026-04-09 21:35:24 +0800

    upgrade nvim.treesitter
    chore: commit save point

--- .config/nvim/lua/plugins/treesitter-textobjects.lua
+++ .config/nvim/lua/plugins/treesitter-textobjects.lua
@@ -1,36 +1,37 @@
--- copied from: https://github.com/josean-dev/dev-environment-files/blob/cb670e8890ca9d8baf978b38ed75987b742032e6/.config/nvim/lua/josean/plugins/nvim-treesitter-text-objects.lua
 return {
+  {
     "nvim-treesitter/nvim-treesitter-textobjects",
+    branch = "master",
     lazy = false,
     dependencies = { "nvim-treesitter/nvim-treesitter" },
     config = function()
-        require("nvim-treesitter.configs").setup({
-            textobjects = {
-                move = {
-                    enable = true,
-                    set_jumps = true, -- whether to set jumps in the jumplist
-                    goto_next_start = {
-                        ["]f"] = { query = "@call.outer", desc = "Next function call start" },
-                        ["]m"] = { query = "@function.outer", desc = "Next method/function def start" },
-                        ["]c"] = { query = "@class.outer", desc = "Next class start" },
-                        ["]i"] = { query = "@conditional.outer", desc = "Next conditional start" },
-                        ["]l"] = { query = "@loop.outer", desc = "Next loop start" },
-                    },
-                    goto_previous_start = {
-                        ["[f"] = { query = "@call.outer", desc = "Prev function call start" },
-                        ["[m"] = { query = "@function.outer", desc = "Prev method/function def start" },
-                        ["[c"] = { query = "@class.outer", desc = "Prev class start" },
-                        ["[i"] = { query = "@conditional.outer", desc = "Prev conditional start" },
-                        ["[l"] = { query = "@loop.outer", desc = "Prev loop start" },
-                    },
-                },
+      require("nvim-treesitter.configs").setup({
+        textobjects = {
+          move = {
+            enable = true,
+            set_jumps = true,
+            goto_next_start = {
+              ["]f"] = { query = "@call.outer", desc = "Next function call start" },
+              ["]m"] = { query = "@function.outer", desc = "Next method/function def start" },
+              ["]c"] = { query = "@class.outer", desc = "Next class start" },
+              ["]i"] = { query = "@conditional.outer", desc = "Next conditional start" },
+              ["]l"] = { query = "@loop.outer", desc = "Next loop start" },
             },
-        })
+            goto_previous_start = {
+              ["[f"] = { query = "@call.outer", desc = "Prev function call start" },
+              ["[m"] = { query = "@function.outer", desc = "Prev method/function def start" },
+              ["[c"] = { query = "@class.outer", desc = "Prev class start" },
+              ["[i"] = { query = "@conditional.outer", desc = "Prev conditional start" },
+              ["[l"] = { query = "@loop.outer", desc = "Prev loop start" },
+            },
+          },
+        },
+      })
 
-        local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
+      local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
 
-        -- vim way: ; goes to the direction you were moving.
-        vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
-        vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
+      vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
+      vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
     end,
+  },
 }
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,29 +1,40 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        event = { "BufReadPost", "BufNewFile" },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    branch = "master",
+    build = ":TSUpdate",
+    lazy = false,
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+          "lua", "go", "c", "cpp", "java", "python", "bash",
+          "sql", "thrift", "json", "jsonnet", "yaml", "toml",
+          "tmux", "rust", "javascript", "typescript", "html",
+        },
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+        indent = { enable = true },
+      })
+    end,
+  },
 
-        config = function()
-            require("nvim-treesitter.configs").setup({
-                ensure_installed = { "lua", "go", "c", "cpp", "java", "python", "bash", "sql", "thrift", "json", "jsonnet", "yaml", "toml", "tmux", "markdown", "rust", "javascript", "typescript", "html" },
-                highlight = { enable = true, additional_vim_regex_highlighting = false },
-                indent = { enable = true },
-            })
-        end,
-    },
-    {
-        "nvim-treesitter/nvim-treesitter-context",
-        event = { "BufReadPost", "BufNewFile" },
-        config = function()
-            require("treesitter-context").setup({ max_lines = 4 })
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    lazy = false,
+    config = function()
+      require("treesitter-context").setup({ max_lines = 4 })
 
-            vim.keymap.set("n", "[k", function()
-                require("treesitter-context").go_to_context(vim.v.count1)
-            end, { silent = true, desc = "treesitter-context: up" })
+      vim.keymap.set("n", "[k", function()
+        require("treesitter-context").go_to_context(vim.v.count1)
+      end, { silent = true, desc = "treesitter-context: up" })
 
-            -- https://www.w3schools.com/colors/colors_picker.asp
-            vim.api.nvim_set_hl(0, "TreesitterContextLineNumber", { bg = "#ff99ff", fg = "#1a1a1a", underline = false })
-        end,
-    },
+      vim.api.nvim_set_hl(0, "TreesitterContextLineNumber", {
+        bg = "#ff99ff",
+        fg = "#1a1a1a",
+        underline = false,
+      })
+    end,
+  },
 }
