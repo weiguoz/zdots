4bb06edfb5b516ef5c3df9fad7016e8cf7b760d1
weiguoz <wg.z@qq.com>
2026-04-23 13:30:44 +0800

    nvim: add copilotchat

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,54 +1,70 @@
 return {
-    'github/copilot.vim',
-    config = function()
-        local opts = { silent = true }
-        vim.keymap.set('i', '<C-f>', 'copilot#Accept("\\<CR>")', {
-            expr = true,
-            replace_keycodes = false
-        })
-        -- https://github.com/github/copilot.vim/blob/a9228e015528c9307890c48083c925eb98a64a79/doc/copilot.txt#L119
-        vim.keymap.set('i', '<C-g>', '<Plug>(copilot-accept-word)', opts) -- must not c-w which is used as delete a back word
-        -- vim.keymap.set('i', '<C-j>', '<Plug>(copilot-next)', opts)
-        -- vim.keymap.set('i', '<C-k>', '<Plug>(copilot-previous)', opts)
-        vim.keymap.set('i', '<C-x>', '<Plug>(copilot-dismiss)', opts)
+    {
+        'github/copilot.vim',
+        config = function()
+            local opts = { silent = true }
+            vim.keymap.set('i', '<C-f>', 'copilot#Accept("\\<CR>")', {
+                expr = true,
+                replace_keycodes = false
+            })
+            -- https://github.com/github/copilot.vim/blob/a9228e015528c9307890c48083c925eb98a64a79/doc/copilot.txt#L119
+            vim.keymap.set('i', '<C-g>', '<Plug>(copilot-accept-word)', opts) -- must not c-w which is used as delete a back word
+            -- vim.keymap.set('i', '<C-j>', '<Plug>(copilot-next)', opts)
+            -- vim.keymap.set('i', '<C-k>', '<Plug>(copilot-previous)', opts)
+            vim.keymap.set('i', '<C-x>', '<Plug>(copilot-dismiss)', opts)
 
-        -- tpope [workaround] https://github.com/community/community/discussions/16800#discussioncomment-2848953
-        -- let g:copilot_node_command = "/opt/homebrew/opt/node@16/bin/node"
-        -- it seems that the above is not needed anymore since 2023-01-06
-        vim.g.copilot_no_tab_map = true
-    end,
+            -- tpope [workaround] https://github.com/community/community/discussions/16800#discussioncomment-2848953
+            -- let g:copilot_node_command = "/opt/homebrew/opt/node@16/bin/node"
+            -- it seems that the above is not needed anymore since 2023-01-06
+            vim.g.copilot_no_tab_map = true
+        end,
+    },
+    {
+        "CopilotC-Nvim/CopilotChat.nvim",
+        branch = "main",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        build = "make tiktoken",
+        opts = {
+            model = "gpt-4.1",
+            temperature = 0.1,
+        },
+        keys = {
+            { "<leader>c", ":CopilotChat<SPACE>", desc = "CopilotChat Prompt" }
+            -- 如果打开对话面边，则可以用 c-s 整段提交等待回答
+        }
+    },
 }
 
 -- 'zbirenbaum/copilot.lua' collpased twice recently, so i turn back to github version(tpope)
 -- 1) https://github.com/zbirenbaum/copilot.lua/issues/408
 -- 2) others
 -- return {
---     'zbirenbaum/copilot.lua',
---     cmd = "Copilot",
---     event = "InsertEnter",
---     config = function()
---         require('copilot').setup({
---             suggestion = {
---                 enabled = true,
---                 auto_trigger = true,
---                 debounce = 75,
---                 keymap = {
---                     accept = "<C-f>",
---                     next = "<C-j>",
---                     prev = "<C-k>",
---                     dismiss = "<C-x>"
---                 },
---             },
---             panel = {
---                 enabled = true,
---                 keymap = {
---                     jump_prev = "[[",
---                     jump_next = "]]",
---                     accept = "<CR>",
---                     refresh = "gr",
---                     open = "<M-CR>"
---                 },
---             },
---         })
---     end,
--- }
+    --     'zbirenbaum/copilot.lua',
+    --     cmd = "Copilot",
+    --     event = "InsertEnter",
+    --     config = function()
+        --         require('copilot').setup({
+            --             suggestion = {
+                --                 enabled = true,
+                --                 auto_trigger = true,
+                --                 debounce = 75,
+                --                 keymap = {
+                    --                     accept = "<C-f>",
+                    --                     next = "<C-j>",
+                    --                     prev = "<C-k>",
+                    --                     dismiss = "<C-x>"
+                    --                 },
+                    --             },
+                    --             panel = {
+                        --                 enabled = true,
+                        --                 keymap = {
+                            --                     jump_prev = "[[",
+                            --                     jump_next = "]]",
+                            --                     accept = "<CR>",
+                            --                     refresh = "gr",
+                            --                     open = "<M-CR>"
+                            --                 },
+                            --             },
+                            --         })
+                            --     end,
+                            -- }
