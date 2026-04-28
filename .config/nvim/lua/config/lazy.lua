db85a1516af1539b105d0b740b6f9ae24d8c8ce6
weiguoz <wg.z@qq.com>
2026-04-28 15:22:18 +0800

    nvim add smart-im: switch input method on entering or leaving insert mode

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -43,6 +43,7 @@ return require('lazy').setup(
         -- db = ~/.config/nvim/cd-project.nvim.json
         -- { "HiPhish/rainbow-delimiters.nvim" }, -- when i run lazy update, it spends a lot of time on this plugin(submodule like test/*)
         { 'mhinz/vim-startify' },
+        { import = 'plugins.smart-im' }, -- icons & indenetation
         -- { import = 'plugins.treesitter-textobjects' },
     }, {
         checker = { enabled = true, frequency = 10 * 86400 },
